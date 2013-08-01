8f5a7381c8cd40a27e099bba4dff4c35d98d8fd6
Jordan Prince <jordanmprince@gmail.com>
2013-08-01 10:04:42 -0500

    first commit

--- models/quote.rb
+++ models/quote.rb
@@ -4,8 +4,40 @@ class Quote < Model
   attr_accessor :total_trip_cost
 
   attr_accessor :travelers
+  
+  # Returns children ordered by age, so oldest is last.
+  #
+  # @return [Travelers] Array of children sorted by age, oldest last.
+  def children
+    travelers.select { |traveler| traveler.is_child? }.sort_by(&:age)
+  end
+  
+  # Returns adults ordered by age, oldest last.
+  #
+  # @return [Travelers] Array of adults sorted by age, oldest last.
+  def adults
+    travelers.select { |traveler| traveler.is_adult? }.sort_by(&:age)
+  end
 
   def premium
-    0.0
+    #no old people
+    return nil if travelers.select { |traveler| traveler.age > MAX_AGE }.first
+    
+    #prepare travelers array by making sure there are only as many children in calculation as adults,
+    #always favoring the oldest to keep around. However, if trip cost is zero, we count children.
+    childrens_array = total_trip_cost == 0 ? children : (children[adults.size..children.length-1] || [])
+    actual_travelers = adults + childrens_array
+    
+    #cycle through each traveler, adding their flat rate for premium.
+    actual_travelers.inject(0) do |premium, traveler|
+      
+      rate = Rate.all.select do |rate|
+        rate.min_age <= traveler.age and rate.max_age >= traveler.age and
+        rate.min_trip_cost <= total_trip_cost/travelers.size
+      end.sort_by(&:max_trip_cost).last
+
+      premium += rate.flat_rate if rate #make sure we atually got it.
+      premium
+    end
   end
 end
--- models/traveler.rb
+++ models/traveler.rb
@@ -1,3 +1,17 @@
 class Traveler < Model
   attr_accessor :age
+  
+  # Returns true if the traveler is a child.
+  #
+  # @return [Boolean] whether or not a child.
+  def is_child?
+    age < 18
+  end
+  
+  # Returns true if the traveler is an adult.
+  #
+  # @return [Boolean] whether or not an adult.
+  def is_adult?
+    age >= 18
+  end
 end
